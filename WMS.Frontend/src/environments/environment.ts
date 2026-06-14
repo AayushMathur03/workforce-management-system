e4d1d37f137313334c241c06c168003d7368590e
AayushMathur03 <mathuraayush03@gmail.com>
2026-06-14 18:10:16 +0530

    Fixing CORS issue

--- WMS.Frontend/src/environments/environment.ts
+++ WMS.Frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://wms-api-aayush-hpepa0etcbg3e4dg.centralindia-01.azurewebsites.net/api'
+  apiUrl: 'https://wms-api-aayush-01-efguhbfaetcdakgz.centralindia-01.azurewebsites.net/api'
 };
